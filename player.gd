f61a7d569e3923234320cf18db60748005da88e2
NanoNero1 <dimitri.kachler@gmail.com>
2025-01-25 13:54:08 +0100

    initial commit

--- player.gd
+++ player.gd
@@ -3,15 +3,20 @@ extends RigidBody2D
 @export var dirMultiplier = 500
 @export var impulsMultiplier = 20000
 
+var debugChurch
+
 var anchor
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anchor = get_tree().get_nodes_in_group("anchor")[0]
+	debugChurch = get_tree().get_nodes_in_group("debugChurch")[0]
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	
 
 	var toAnchor = anchor.position - position
 	var anchorDist = toAnchor.length()
@@ -22,10 +27,24 @@ func _process(delta):
 	
 	var dir=Vector2.ZERO
 	var impuls=Vector2.ZERO
+	
+	# Change direction so that the sprite is "standing" on the planet
+	set_rotation(toAnchor.angle() - PI/2)
+	
+	#print('lol')
+	var normalVector = (Vector2.UP.rotated( transform.get_rotation())).normalized()
+	
+	#print(normalVector)
+	var space_state = get_world_2d().direct_space_state
+	
+	var query = PhysicsRayQueryParameters2D.create(position, position - normalVector*80 )
+	debugChurch.position =  position - normalVector*80
+	var result = space_state.intersect_ray(query)
+	print(result)
 
 	if Input.is_action_just_pressed("Inthebubble"): #need to check if is on the ground
 		impuls=toAnchor
-	if Input.is_action_just_pressed("Jump") : #need to check if is not in the air already
+	if Input.is_action_just_pressed("Jump") and (result != {}): #need to check if is not in the air already
 		impuls=-toAnchor
 	if Input.is_action_pressed("Left"):
 		dir=-Vector2(toAnchor.y,-toAnchor.x)
@@ -36,5 +55,4 @@ func _process(delta):
 	var totalForce = anchorForce + dirForce + impulsForce
 	apply_force(totalForce)
 	
-	# Change direction so that the sprite is "standing" on the planet
-	set_rotation(toAnchor.angle() - PI/2)
+	
