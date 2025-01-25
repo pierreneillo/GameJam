70c0343900508b0ede17e8bd472eca79e42fa7f8
NanoNero1 <dimitri.kachler@gmail.com>
2025-01-25 14:33:09 +0100

    test

--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ extends RigidBody2D
 @export var impulsMultiplier = 20000
 
 var debugChurch
+var bulletScene = preload("res://bullet.tscn")
 
 var anchor
 # Called when the node enters the scene tree for the first time.
@@ -19,6 +20,7 @@ func _process(delta):
 	
 
 	var toAnchor = anchor.position - position
+	var toMouse = get_viewport().get_mouse_position() - position
 	var anchorDist = toAnchor.length()
 	var forceMultiplier = anchorDist
 	
@@ -31,28 +33,41 @@ func _process(delta):
 	# Change direction so that the sprite is "standing" on the planet
 	set_rotation(toAnchor.angle() - PI/2)
 	
-	#print('lol')
 	var normalVector = (Vector2.UP.rotated( transform.get_rotation())).normalized()
-	
-	#print(normalVector)
 	var space_state = get_world_2d().direct_space_state
 	
 	var query = PhysicsRayQueryParameters2D.create(position, position - normalVector*80 )
 	debugChurch.position =  position - normalVector*80
 	var result = space_state.intersect_ray(query)
-	print(result)
+	
+	var isOnGround = (result != {})
 
 	if Input.is_action_just_pressed("Inthebubble"): #need to check if is on the ground
 		impuls=toAnchor
-	if Input.is_action_just_pressed("Jump") and (result != {}): #need to check if is not in the air already
+	if Input.is_action_just_pressed("Jump") and isOnGround: #need to check if is not in the air already
 		impuls=-toAnchor
-	if Input.is_action_pressed("Left"):
+	if Input.is_action_pressed("Left") and (isOnGround):
 		dir=-Vector2(toAnchor.y,-toAnchor.x)
-	if Input.is_action_pressed("Right"):
+	if Input.is_action_pressed("Right") and (isOnGround):
 		dir=Vector2(toAnchor.y,-toAnchor.x)
+		
+	if Input.is_action_just_pressed("click"):
+		var b = bulletScene.instantiate()
+		get_tree().get_current_scene().add_child(b)
+		
+
+		
+		# Set the position and impulse
+		
+		b.position = position + toMouse.normalized()*100
+		b.rotation = toMouse.angle() + PI/2
+		b.apply_force(toMouse.normalized()*100000)
 	var dirForce=dir.normalized()*dirMultiplier
 	var impulsForce=impuls.normalized()*impulsMultiplier
-	var totalForce = anchorForce + dirForce + impulsForce
+	
+	var dragForce = -linear_velocity.normalized() * 10
+	
+	var totalForce = anchorForce + dirForce + impulsForce + dragForce
 	apply_force(totalForce)
 	
 	
