4afff339df1791011be78b84be0591503db28d46
Gwendal Grison <gwendal.grison@laposte.net>
2025-01-26 10:21:18 +0100

    ajout des pv et shader pour flasher quand touché par ennemis, ennemis réparés

--- player.gd
+++ player.gd
@@ -4,28 +4,64 @@ extends RigidBody2D
 @export var impulsMultiplier = 20000
 @export var maxJumps = 2
 @export var maxSpeed = 450
-@export var k = 200
+@export var k = 500000
 @export var marge = 30 
+@export var maxSlopeAngle = TAU/6
+@export var invicibilityTimeMax=3
 
 var debugChurch
 var bulletScene = preload("res://bullet.tscn")
 
 var anchor
 var nbJumps=0
-
+var on_floor: bool = false
+var hearts: int = 3
+var invicibility: = false
+var invicibilityTime=0
+var spriteDefault
+var spriteFlying
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anchor = get_tree().get_nodes_in_group("anchor")[0]
 	debugChurch = get_tree().get_nodes_in_group("debugChurch")[0]
-	#$HUD.visible=true
+	$"../HUD".visible=true
+	self.connect("body_entered", Callable(self,"_on_body_shape_entered"))
+	spriteDefault=get_node("LR")
+	spriteFlying=get_node("flyin")
+
+	
+func _on_body_shape_entered(body):
+	if body.get_collision_layer_value(2) and invicibility==false:  # Vérifie si l'objet appartient à la layer 3
+		print("touché")
+		var H=get_node("../HUD/Heart"+str(hearts))
+		H.visible=false
+		hearts-=1
+		invicibility=true
+		spriteDefault.material.set_shader_parameter("solid_color",Color.WHITE)
+		spriteFlying.material.set_shader_parameter("solid_color",Color.WHITE)
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var i := 0
+	var up = (global_position - anchor.global_position).normalized()
+	on_floor=false
+	while i < state.get_contact_count():
+		var normal := state.get_contact_local_normal(i)
+		on_floor = normal.dot(up) > cos(maxSlopeAngle) # this can be dialed in
+		#  1.0 would be perfectly straight up
+		#  0.0 is a wall
+		# -1.0 is a ceiling
+		i += 1
+		if on_floor:
+			break
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-
+	print(on_floor)
 	var toAnchor = anchor.position - position
 	var toMouse = get_viewport().get_mouse_position() - position
 	var anchorDist = toAnchor.length()
-	var forceMultiplier = 1000 # k/anchorDist
+	var forceMultiplier = k/anchorDist
 
 	# Gravity model for now, independant of the distance to the center of the bubble
 	var anchorForce = toAnchor.normalized() * forceMultiplier
@@ -35,22 +71,22 @@ func _physics_process(delta):
 
 	# Change direction so that the sprite is "standing" on the planet
 	set_rotation(toAnchor.angle() - PI/2)
-
+	'''
 	var normalVector = (Vector2.UP.rotated( transform.get_rotation())).normalized()
 	var space_state = get_world_2d().direct_space_state
 
 	var query = PhysicsRayQueryParameters2D.create(position, position - normalVector*marge )
 	debugChurch.position =  position - normalVector*80
 	var result = space_state.intersect_ray(query)
-
+	var isOnGround = (result != {}) '''
 	var bulletRecoil = Vector2(0,0)
 
 	var frogAnim = 0
-	var isOnGround = (result != {})
+
 
 	if Input.is_action_just_pressed("Inthebubble"): #need to check if is on the ground
 		impuls=toAnchor
-	if Input.is_action_just_pressed("Jump") and (isOnGround or nbJumps<maxJumps): #need to check if is not in the air already
+	if Input.is_action_just_pressed("Jump") and (on_floor or nbJumps<maxJumps): #need to check if is not in the air already
 		impuls=-toAnchor
 		nbJumps+=1
 	if Input.is_action_pressed("Left"):
@@ -60,7 +96,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Right"):
 		frogAnim = 1
 		dir=Vector2(toAnchor.y,-toAnchor.x)
-	if isOnGround and nbJumps>=maxJumps:
+	if on_floor and nbJumps>=maxJumps:
 		nbJumps=0
 
 	if Input.is_action_just_pressed("click"):
@@ -73,25 +109,25 @@ func _physics_process(delta):
 		bulletRecoil = -toMouse.normalized()*10000
 	
 	
-	if not isOnGround:
+	if not on_floor:
 		frogAnim = 2
 	
 	if frogAnim == 0:
 		# Left
-		get_child(0).get_child(0).flip_h = true
+		spriteDefault.flip_h = true
 		
-		get_child(0).get_child(0).visible = true
-		get_child(0).get_child(1).visible = false
+		spriteDefault.visible = true
+		spriteFlying.visible = false
 	elif frogAnim == 1:
 		# Right
-		get_child(0).get_child(0).flip_h = false
+		spriteDefault.flip_h = false
 		
-		get_child(0).get_child(0).visible = true
-		get_child(0).get_child(1).visible = false
+		spriteDefault.visible = true
+		spriteFlying.visible = false
 	else: 
-		get_child(0).get_child(0).visible = false
-		get_child(0).get_child(1).visible = true
-		get_child(0).get_child(1).rotation = toMouse.angle() - rotation + PI/2
+		spriteDefault.visible = false
+		spriteFlying.visible = true
+		spriteFlying.rotation = toMouse.angle() - rotation + PI/2
 		
 		
 		# Flying
@@ -110,4 +146,10 @@ func _physics_process(delta):
 		var excess_speed = linear_velocity.length() - maxSpeed
 		var braking_force = -linear_velocity.normalized() * excess_speed * 10
 		apply_force(braking_force)
-	
+	if invicibility and invicibilityTime<invicibilityTimeMax:
+		invicibilityTime+=delta
+	if invicibility and invicibilityTime>=invicibilityTimeMax:
+		invicibilityTime=0
+		invicibility=false
+		spriteDefault.material.set_shader_parameter("solid_color",Color.BLACK)
+		spriteFlying.material.set_shader_parameter("solid_color",Color.BLACK)
